a267099369d404b15c6ce8c15bf93a1a09917c47
Eugene Kravec <krava255@gmail.com>
2022-05-21 23:02:03 +0300

    Promisification

--- async.js
+++ async.js
@@ -4,16 +4,42 @@ const fs = require("fs");
 
 const PORT = 3004;
 
-const server = http.createServer((request, response) => {
+const delay = (ms) => {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res()
+        }, ms);
+
+    })
+}
+
+const readFile = (path) => {
+    return new Promise((res, rej) => {
+        fs.readFile(path, (err, data) => {
+            if (err) rej(err)
+            else res(data)
+        })
+    })
+}
+
+const server = http.createServer(async (request, response) => {
 
     switch (request.url) {
         case '/home':
-            fs.readFile('pages/indecx.html', (err, data) => {
-                if (err) response.write('500 Some error occured')
-                else response.write(data);
+            try {
+                const data = await readFile('pages/index.html')
+                response.write(data);
                 response.end();
-            })
+            } catch (err) {
+                response.write('Some ERRRRRORR!!!');
+                response.end();
+            }
 
+            break;
+        case '/about':
+            await delay(3000)
+            response.write('Hello Favicon from about');
+            response.end();
             break;
         default:
             response.write('404 NOT FOUND');
